4233aad5613317babc6c8096cd9892a4597a5d62
liban Abdullahi <libanr4243@gmail.com>
2022-02-02 21:37:53 +0300

    displaying list of agreement documents and updating agreement controller. closes #14

--- Document-Management-Web/src/main/java/com/springframework/documentmanagementsystem/controller/AgreementController.java
+++ Document-Management-Web/src/main/java/com/springframework/documentmanagementsystem/controller/AgreementController.java
@@ -1,5 +1,6 @@
 package com.springframework.documentmanagementsystem.controller;
 
+import com.springframework.documentmanagementsystem.services.AgreementDocumentsServices;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AgreementController {
 
+    private final AgreementDocumentsServices agreementDocumentsServices;
+
+    public AgreementController(AgreementDocumentsServices agreementDocumentsServices) {
+        this.agreementDocumentsServices = agreementDocumentsServices;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
     public String getDocuments(Model model){
+        model.addAttribute("listDocuments", agreementDocumentsServices.findAll());
         return "agreement/index";
     }
 }
