2c75a2ff439afc56f33c7645a231345f00fe134d
liban Abdullahi <libanr4243@gmail.com>
2022-02-14 19:12:15 +0300

    converted Agreement Document To Spring Data JPA Entity. closes #19.

--- Document-Management-Data/src/main/java/com/springframework/documentmanagementsystem/models/AgreementDocuments.java
+++ Document-Management-Data/src/main/java/com/springframework/documentmanagementsystem/models/AgreementDocuments.java
@@ -1,12 +1,24 @@
 package com.springframework.documentmanagementsystem.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "agreement_documents")
 public class AgreementDocuments extends Documents{
 
+    @Column(name = "type_agreement")
     private String typeAgreement;
+
+    @Column(name = "deadline_agreement")
     public LocalDate deadlineAgreement;
+
+    @Column(name = "contractor")
     private String contractor;
+
+    @Column(name = "amount")
     private long amount;
 
     public String getTypeAgreement() {
