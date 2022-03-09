910bb8f366aa0d25cc5cfc64ad628738ec4623fa
liban Abdullahi <libanr4243@gmail.com>
2022-03-09 20:08:33 +0300

    Implemented displaying post details. #44

--- Document-Management-Web/src/main/java/com/springframework/documentmanagementsystem/controller/MessageController.java
+++ Document-Management-Web/src/main/java/com/springframework/documentmanagementsystem/controller/MessageController.java
@@ -4,6 +4,7 @@ import com.springframework.documentmanagementsystem.services.MessageServices;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/posts")
@@ -20,5 +21,11 @@ public class MessageController {
     public String getMessages(Model model){
         model.addAttribute("listPosts", messageServices.findAll());
         return "messages/posts";
+    }
+
+    @GetMapping("/{id}/show")
+    public String showPost(@PathVariable Long id, Model model){
+        model.addAttribute("post", messageServices.findById(id));
+        return "messages/postDetails";
     }
 }
